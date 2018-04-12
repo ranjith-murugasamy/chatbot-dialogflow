a007f1a8b336f102d90679584bd48ab83c7326fe
Ranjith Murugasamy <ranjith@intellectyx.com>
2018-04-12 12:18:47 +0530

    First Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+ // token: 'AIzaSyDFN-h_BS3T_nwHxKK-bQQgK8f8CUJV_js'
+  token: '7c5e72a76db948fe9702ea77cc052e57'
+};
